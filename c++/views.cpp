64c7eeabbdb15fafc13845f89c069a14ab6d88fe
shadiyarzhaqsylyq <shadiyar.zhaqsylyq@gmail.com>
2026-08-31 02:06:57 +0500

    Create views.cpp

---
+++ c++/views.cpp
@@ -0,0 +1,3 @@
+//std::string_view,<string_view> header, replaces [const char*],read only access to strings or substrings without copying memory
+//std::span,<span> header,replaces [*T ptr,size_t,len], read/writes access to contiguous sequence of objects(vector or raw array)
+//std::views,<ranges> header, replaces [complex iterator loops]
