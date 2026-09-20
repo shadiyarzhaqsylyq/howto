756de130fcc740fc1422c9d32feeda2d3c9d171f
shadiyarzhaqsylyq <shadiyar.zhaqsylyq@gmail.com>
2026-09-20 12:49:41 +0500

    Update smartptrs.cpp

--- c++/smartptrs.cpp
+++ c++/smartptrs.cpp
@@ -9,3 +9,5 @@ std::vector<std::unique_ptr<Widget>> widgets;
 auto w = std::make_unique<Widget>();
 
 widgets.push_back(std::move(w)); // 'w' is now empty; vector owns it
+
+// Do not use smart pointers with int, double and self managing objects std::string, vector keep them on the stack
