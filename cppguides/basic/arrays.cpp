c69b92656ac11f5298fd88b1dd760ae9032506bd
shadiyarzhaqsylyq <shadiyar.zhaqsylyq@gmail.com>
2026-09-22 10:18:35 +0500

    Create arrays.cpp

---
+++ cppguides/basic/arrays.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iterator>
+#include <array>
+void print_nums(const std::array<int, 3u> & numbers){
+
+    for(const auto number: numbers) //<1>
+    {	
+		std::cout << std::noshowpos;
+        std::cout << number << '\n'; //<2>
+    }
+
+}
+
+int main() 
+{
+    std::array<int, 3u> v = {3, 1, 4};
+    auto vi = std::begin(v);
+    std::cout << std::showpos << *vi << '\n'; 
+    
+    int a[] = {-5, 10, 15};
+    auto ai = std::begin(a);
+    std::cout << *ai << '\n';
+	print_nums(v);
+return 0;
+}
