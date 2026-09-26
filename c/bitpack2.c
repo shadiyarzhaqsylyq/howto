c5ed2c2364d62ddff9c561138b3f3362b4edfc46
shadiyarzhaqsylyq <shadiyar.zhaqsylyq@gmail.com>
2026-09-26 08:16:23 +0500

    Update bitpack2.c

--- c/bitpack2.c
+++ c/bitpack2.c
@@ -10,7 +10,7 @@ typedef uint64_t PageID;
 // 1. GENERATE PageID (Guaranteed Bit 63 = 0)
 static inline PageID make_page_id(uint32_t table_id, uint32_t page_no) {
     // Assert table_id fits in 31 bits so it never touches bit 63
-    assert((table_id & ~TABLE_ID_MASK) == 0 && "table_id exceeds 31 bits!");
+    assert((table_id & ~TABLE_ID_MASK) == 0 && "table_id exceeds 31 bits!"); // If program crashes it outputs the string
     
     return (((uint64_t)table_id & TABLE_ID_MASK) << 32) | ((uint64_t)page_no & PAGE_NO_MASK);
 }
